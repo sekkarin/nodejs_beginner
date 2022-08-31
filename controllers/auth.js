3596a1a125b5b0d8397e9b79b12e421564118b58
sekkarin <sekkri1234@gmail.com>
2022-08-31 12:16:50 +0700

    31/8/2265 setcookie login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'login',
-        isAuthenticated:req.isLoggedIn
-    })
-}
+    const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
+    res.render("auth/login", {
+        path: "/login",
+        pageTitle: "login",
+        isAuthenticated: isLoggedIn,
+    });
+};
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true
-    res.redirect('/')
-}
+    res.setHeader("Set-Cookie", "loggedIn=true");
+    res.redirect("/");
+};
