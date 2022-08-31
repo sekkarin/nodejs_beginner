222f55e00e95bc4117d51dd380ddce3871817511
sekkarin <sekkri1234@gmail.com>
2022-08-31 11:52:28 +0700

    add auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'login'
+        pageTitle: 'login',
+        isAuthenticated:req.isLoggedIn
     })
 }
 exports.postLogin = (req, res, next) => {
