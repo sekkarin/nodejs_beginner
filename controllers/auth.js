d60394396535e8b7ce492df11ec9a099b9f05ef5
sekkarin <sekkri1234@gmail.com>
2022-09-03 17:14:13 +0700

    encrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs')
 exports.getLogin = (req, res, next) => {
 
     res.render("auth/login", {
@@ -26,27 +27,35 @@ exports.postLogout = (req, res, next) => {
     });
 }
 exports.postSignup = (req, res, next) => {
+    console.log("sign up");
     const email = req.body.email
     const password = req.body.password
     const name = req.body.name
+    console.log(`${email} , ${password} , ${name}`);
     const confirmPassword = req.body.confirmPassword
-    User.findOne({email: email})
-    .then(userDoc => {
-        if(userDoc){
-            return res.redirect('/signup')
-        }
-        const user = new User({
-            name:name,
-            email:email,
-            password:password,
-            cart:{items:[]}
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup')
+            }
+            return bcrypt.hash(password, 12)
+
         })
-        return user.save()
-    })
-    .thent(result=>{
-        res.redirect('/login')
-    })
-    .catch(err => console.log(err))
+        .then(hashPwd => {
+            const user = new User({
+                name: name,
+                email: email,
+                password: hashPwd,
+                cart: { items: [] }
+            })
+            return user.save()
+        }
+
+        )
+        .then(result => {
+            res.redirect('/login')
+        })
+        .catch(err => console.log(err))
 }
 exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
